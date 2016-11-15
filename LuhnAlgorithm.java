ddf173bfac4ea5b790bc32861d97829614d979a1
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-16 00:12:15 +0100

    Create LuhnAlgorithm.java

---
+++ LuhnAlgorithm.java
@@ -0,0 +1,25 @@
+package luhnalgorithm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LuhnAlgorithm extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LuhnAlgorithmFXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Sprawdzanie poprawności PESEL");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
