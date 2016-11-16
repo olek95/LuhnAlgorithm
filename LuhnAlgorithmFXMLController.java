c832a6f1c97bca22b4fe82259191bc07ea268388
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-16 02:06:42 +0100

    Update LuhnAlgorithmFXMLController.java
    dodanie zapisu do pliku

--- LuhnAlgorithmFXMLController.java
+++ LuhnAlgorithmFXMLController.java
@@ -1,12 +1,17 @@
 package luhnalgorithm;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -41,7 +46,10 @@ public class LuhnAlgorithmFXMLController implements Initializable {
             resultTextArea.appendText("Suma wszystkich dotychczasowych wyników: " + sum + "\n");
             parity = !parity;
         }
-        if(sum % 10 == 0) resultTextArea.appendText("PESEL poprawny");
+        if(sum % 10 == 0){
+            resultTextArea.appendText("PESEL poprawny");
+            writeToFile(bPersonalIdentityNumber);
+        }
         else resultTextArea.appendText("PESEL niepoprawny, bo modulo wynosi: " + (sum % 10) + "!");
     }
     
@@ -51,4 +59,11 @@ public class LuhnAlgorithmFXMLController implements Initializable {
             System.exit(0);
         });
     }    
+    private void writeToFile(byte[] bPersonalIdentityNumber){
+        try{
+            Files.write(Paths.get("Wynik.txt"), bPersonalIdentityNumber, StandardOpenOption.APPEND);
+        }catch(IOException e){
+            Logger.getLogger(LuhnAlgorithmFXMLController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 }
